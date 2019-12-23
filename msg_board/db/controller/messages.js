dcf55c53e230ee7ccbc672862e6a73d9dbee5b9f
Sheryleen <sheryholthaus@yahoo.com>
2019-12-22 19:35:44 -0700

    updated controller

--- msg_board/db/controller/messages.js
+++ msg_board/db/controller/messages.js
@@ -1,25 +1,43 @@
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex("messages")
+const knex = require("../knex");
+
+exports.getAllMessages = (req, res) => {
+  knex //instance of knex
+    .select() //select all
+    .table("messages") //from name
+    .then(messages => res.json(messages)); //getting all users back
+};
+
+exports.getOneMessage = (req, res) => {
+  knex("message")
+    .where("id", req.params.id)
+    .then(message => res.json(message));
+};
+
+exports.addOneMessage = (req, res) => {
+  knex("message")
+    .insert({
+      ...req.body //all column data in row
+    })
+
+    .returning("*")
+    .then(message => res.json([message]));
+};
+
+exports.updateOneMessage = (req, res) => {
+  knex("message")
+    .update({
+      ...req.body //all column data in row
+    })
+
+    .where("id", req.params.id)
+    .returning("*")
+    .then(updatedMessage => res.json(updatedMessage));
+};
+
+exports.removeOneMessage = (req, res) => {
+  knex("message")
     .del()
-    .then(function() {
-      // Inserts seed entries
-      return knex("messages").insert([
-        {
-          
-          name: "Criminal",
-          message: "What Are You?",
-          created_at: " ",
-          updated_at: " ",
-          
-          },
-          {
-          
-          name: "Batman",
-          message: "I\"m Batman",
-          created_at: " ",
-          updated_at: " ",
-          
-        },
-
-
+    .where("id", req.params.id)
+    .returning("*")
+    .then(newMessage => res.json(newMessage)); //returns removed name
+};
